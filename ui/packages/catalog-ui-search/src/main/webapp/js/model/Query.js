880c2ff4207a4f3c397f51b6767d9dc7f4835b51
Joshua Mack <joshua.mack@connexta.com>
2018-02-06 17:02:10 -0700

    Fixed issue with saving schedule triggering workspace unsaved

--- ui/packages/catalog-ui-search/src/main/webapp/js/model/Query.js
+++ ui/packages/catalog-ui-search/src/main/webapp/js/model/Query.js
@@ -91,13 +91,6 @@ define([
                     isLocal: false,
                     isOutdated: false,
                     schedules: []
-                    // isScheduled: false,
-                    // scheduleAmount: 1,
-                    // scheduleUnit: 'weeks',
-                    // scheduleStart: '',
-                    // scheduleEnd: '',
-                    // subscribedUsers: []
-
                 }, user.getQuerySettings().toJSON());
             },
             resetToDefaults: function() {
@@ -120,6 +113,7 @@ define([
                 this.set('id', this.getId());
                 this.listenTo(user.get('user>preferences'), 'change:resultCount', this.handleChangeResultCount);
                 this.listenTo(this, 'change:cql', () => this.set('isOutdated', true));
+                this.listenTo(this.get('schedules'), 'change add remove update', this.handleSchedulesChange);
             },
             buildSearchData: function () {
                 var data = this.toJSON();
@@ -364,6 +358,9 @@ define([
                 if (this.get('result')) {
                     this.get('result').resetResultCountsBySource();
                 }
+            },
+            handleSchedulesChange: function() {
+                this.trigger('update');
             },
             getResultsRangeLabel: function (resultsCollection) {
                 var results = resultsCollection.fullCollection.length;
