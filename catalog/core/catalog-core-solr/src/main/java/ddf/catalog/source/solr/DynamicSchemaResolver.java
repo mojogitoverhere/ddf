8adbcf6b45db7bd9dc39b7b10be085a7ff6a7cc0
Phillip Klinefelter <phillip.klinefelter@gmail.com>
2018-07-10 10:56:53 -0700

    Revert "DDF-3938 Use Solr 7 DocValues to replace sort key fields"
    This reverts commit 303768a4dfd6d68ea5b316eba4e473e8811a88a1.

--- catalog/core/catalog-core-solr/src/main/java/ddf/catalog/source/solr/DynamicSchemaResolver.java
+++ catalog/core/catalog-core-solr/src/main/java/ddf/catalog/source/solr/DynamicSchemaResolver.java
@@ -28,6 +28,7 @@ import ddf.catalog.data.AttributeType.AttributeFormat;
 import ddf.catalog.data.Metacard;
 import ddf.catalog.data.MetacardCreationException;
 import ddf.catalog.data.MetacardType;
+import ddf.catalog.data.Result;
 import ddf.catalog.data.impl.AttributeDescriptorImpl;
 import ddf.catalog.data.impl.BasicTypes;
 import ddf.catalog.data.impl.MetacardImpl;
@@ -329,11 +330,22 @@ public class DynamicSchemaResolver {
             attributeValues = byteArrays;
           }
 
-          if (AttributeFormat.GEOMETRY.equals(format)
-              && solrInputDocument.getFieldValue(formatIndexName + SchemaFields.SORT_SUFFIX)
-                  == null) {
-            solrInputDocument.addField(
-                formatIndexName + SchemaFields.SORT_SUFFIX, createCenterPoint(attributeValues));
+          if (solrInputDocument.getFieldValue(formatIndexName + SchemaFields.SORT_KEY_SUFFIX)
+              == null) {
+            if (AttributeFormat.GEOMETRY.equals(format)) {
+              solrInputDocument.addField(
+                  formatIndexName + SchemaFields.SORT_KEY_SUFFIX,
+                  createCenterPoint(attributeValues));
+            } else if (AttributeFormat.STRING.equals(format)) {
+              solrInputDocument.addField(
+                  formatIndexName + SchemaFields.SORT_KEY_SUFFIX,
+                  formatSortKey((String) attributeValues.get(0)));
+            } else if (!(AttributeFormat.BINARY.equals(format)
+                || AttributeFormat.OBJECT.equals(format)
+                || AttributeFormat.XML.equals(format))) {
+              solrInputDocument.addField(
+                  formatIndexName + SchemaFields.SORT_KEY_SUFFIX, attributeValues.get(0));
+            }
           }
 
           // Prevent adding a field already on document
@@ -387,6 +399,10 @@ public class DynamicSchemaResolver {
     }
 
     solrInputDocument.addField(SchemaFields.METACARD_TYPE_OBJECT_FIELD_NAME, metacardTypeBytes);
+  }
+
+  private String formatSortKey(String term) {
+    return StringUtils.substring(term.trim(), 0, 127).toLowerCase();
   }
 
   /*
@@ -562,7 +578,8 @@ public class DynamicSchemaResolver {
   }
 
   public boolean isPrivateField(String solrFieldName) {
-    return PRIVATE_SOLR_FIELDS.contains(solrFieldName);
+    return PRIVATE_SOLR_FIELDS.contains(solrFieldName)
+        || solrFieldName.endsWith(SchemaFields.SORT_KEY_SUFFIX);
   }
 
   /**
@@ -870,8 +887,11 @@ public class DynamicSchemaResolver {
   }
 
   public String getSortKey(String field) {
-    if (field.endsWith(SchemaFields.GEO_SUFFIX)) {
-      field = field + SchemaFields.SORT_SUFFIX;
+    if (!(field.endsWith(SchemaFields.SORT_KEY_SUFFIX)
+        || Result.DISTANCE.equals(field)
+        || Result.RELEVANCE.equals(field)
+        || Result.TEMPORAL.equals(field))) {
+      field = field + SchemaFields.SORT_KEY_SUFFIX;
     }
     return field;
   }
